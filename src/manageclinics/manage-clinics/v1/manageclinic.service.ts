92f12d24901cdd6478099f6be8cb5e53772884fa
raviraj879214 <ravirajalphainfo@gmail.com>
2026-09-01 11:15:56 +0530

    update

--- src/manageclinics/manage-clinics/v1/manageclinic.service.ts
+++ src/manageclinics/manage-clinics/v1/manageclinic.service.ts
@@ -1171,70 +1171,16 @@ export class ManageClinicServices implements IManageClinic{
     }
 
 
-
 async deleteClinic(uuid: string) {
     return this.prisma.$transaction(
         async (tx) => {
 
-            await tx.clinicDoctorAddress.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicDoctor.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicGoogleAccount.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicListingBoost.deleteMany({
-                where: { clinicId: uuid },
-            });
-
-            await tx.clinicPackage.deleteMany({
-                where: { clinicId: uuid },
-            });
-
-            await tx.clinicRatingSummary.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicSpecialization.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicSpecialty.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicTreatment.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.clinicBoost.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.googleReview.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
-            await tx.patientQueryFinalPrice.deleteMany({
-                where: { clinicId: uuid },
-            });
-
-            await tx.patientQuery.deleteMany({
-                where: { clinicId: uuid },
-            });
-
-            await tx.hospitalLicense.deleteMany({
-                where: { clinicUuid: uuid },
-            });
-
             return tx.clinic.delete({
-                where: { uuid : uuid },
+                where: {
+                    uuid: uuid,
+                },
             });
+
         },
         {
             maxWait: 10000,
@@ -1242,8 +1188,6 @@ async deleteClinic(uuid: string) {
         },
     );
 }
-    
-
     
 
 }
